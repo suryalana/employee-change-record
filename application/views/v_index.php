acf5192acc0e54ab29ca7e426c362d75d858759b
suryalana <alipsayyidah102@gmail.com>
2021-10-26 10:41:41 +0700

    Initial commit

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -18,7 +18,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-	<title>ECR</title>
+	<title>Cladtek</title>
 </head>
 
 <body>
